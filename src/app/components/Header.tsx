a5c366685ddf0935447eb9c6921f2065b3fa6a63
Lauren Yeung <laurenyeung217@gmail.com>
2024-10-05 20:28:09 +0000

    Feat: Integrate session handling and Nylas api
    - Added `next-app-session` to manage user sessions
    - Integrated session-based email retrieval in `Header.tsx`
    - Added conditional rendering of the dashboard and logout links based on session email
    - Installed `nylas` for future integration with Nylas API (email, calendar, etc.)

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,10 @@
+'use server';
+import { session } from "@/libs/session";
 import { CalendarDays } from "lucide-react";
 import Link from "next/link";
 
-export default function Header() {
+export default async function Header() {
+    const email = await session().get('email');
     return (
         <header className="flex gap-4 justify-between py-6 text-gray-600">
             <div className="flex items-center gap-10">
@@ -15,11 +18,21 @@ export default function Header() {
                     <Link href={'/pricing'}>Pricing</Link>
                 </nav>
             </div>
+            {email && (
+                <nav className="flex items-center gap-4">
+                    <Link href={'/about'} className="bg-blue-600 text-white py-2 px-4 rounded-full">
+                        Dashboard
+                    </Link>
+                    <Link href={'/api/logout'}>Logout</Link>
+                </nav>
+            )}
+            {!email && (
+                <nav className="flex items-center gap-4">
+                    <Link href={'/features'}>Sign in</Link>
+                    <Link href={'/about'} className="bg-blue-600 text-white py-2 px-4 rounded-full">Get started</Link>
+                </nav>
+            )}
 
-            <nav className="flex items-center gap-4">
-                <Link href={'/features'}>Sign in</Link>
-                <Link href={'/about'} className="bg-blue-600 text-white py-2 px-4 rounded-full">Get started</Link>
-            </nav>
         </header>
     );
 }
